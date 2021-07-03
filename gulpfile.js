1543c6587eabf4cc9f353a56d11365af863d47e6
ubsknr <34959208+ubsknr@users.noreply.github.com>
2021-07-03 18:11:06 +0300

    component help sidebar list

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="1-search-filter-components";
+const fileName="4-support-components";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
