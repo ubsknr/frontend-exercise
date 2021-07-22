aaad3f7e2b69d6387c52435cd3e3a7039a85d15b
ubsknr <34959208+ubsknr@users.noreply.github.com>
2021-07-22 07:52:55 +0300

    13-BlogCards uygulaması gerçekleştirildi.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="12-TrialCard";
+const fileName="13-BlogCards";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
