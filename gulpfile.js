6adde7e34d6a02e46db602cb398581f9c7b7e406
ubsknr <34959208+ubsknr@users.noreply.github.com>
2021-07-25 14:31:01 +0300

    15-ProfilPageComponent uygulaması gerçekleştirildi.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="14-SettingPopup";
+const fileName="15-ProfilePageComponents";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
